69465af5444da0b91fa7e7da3d370af09b2373d1
pwj <kissbabypwj@sina.com>
2020-03-10 18:12:16 +0800

    修改CameraType界面

--- PJVideoDemo/Camera/View/PJCameraTypeView.m
+++ PJVideoDemo/Camera/View/PJCameraTypeView.m
@@ -14,7 +14,7 @@
     CGPoint _prePoint;
     CGPoint _startPoint;
     CGFloat _buttonWidth;
-    NSInteger _modelCount;
+    NSArray *_titles;
 }
 
 @end
@@ -31,33 +31,42 @@
 -(void)setUpView
 {
     NSArray * titles = @[@"拍照",@"拍15秒",@"拍30秒"];
-    _modelCount = titles.count;
+    _titles = titles;
     _buttonWidth = 80;
 
-    CGFloat backgroundViewWidth = _buttonWidth * titles.count;
-    UIView * backgroundView = [UIView new];
-    backgroundView.frameWidth  = backgroundViewWidth;
-    backgroundView.frameHeight = self.frameHeight;
-    backgroundView.frameY      = 0;
-    backgroundView.frameX      = ([UIScreen mainScreen].bounds.size.width - backgroundViewWidth) / 2 + _buttonWidth;
+    [self setUpBackgroundView];
+    
+    [self setUpModelButton];
+    
+    UIPanGestureRecognizer * panGes = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesAction:)];
+    [self addGestureRecognizer:panGes];
+}
+
+-(void)setUpBackgroundView
+{
+    CGFloat backgroundViewWidth = _buttonWidth * [self titleCount];
+    UIView * backgroundView     = [UIView new];
+    backgroundView.frameWidth   = backgroundViewWidth;
+    backgroundView.frameHeight  = self.frameHeight;
+    backgroundView.frameY       = 0;
+    backgroundView.frameX       = ([UIScreen mainScreen].bounds.size.width - backgroundViewWidth) / 2 + _buttonWidth;
     [self addSubview:backgroundView];
     _backgroundView = backgroundView;
-    
+}
+
+-(void)setUpModelButton
+{
     CGFloat buttonWidth  = _buttonWidth;
     CGFloat buttonHeight = _backgroundView.frameHeight;
-  
-    for (NSUInteger i = 0; i < titles.count; i++) {
+    
+    for (NSUInteger i = 0; i < [self titleCount]; i++) {
         UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
         button.frame      = CGRectMake(i * buttonWidth, 0, buttonWidth, buttonHeight);
-        [button setTitle:titles[i] forState:UIControlStateNormal];
+        [button setTitle:_titles[i] forState:UIControlStateNormal];
         [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [button addTarget:self action:@selector(typeButtonAction:) forControlEvents:UIControlEventTouchUpInside];
         [_backgroundView addSubview:button];
     }
-    
-    
-    UIPanGestureRecognizer * panGes = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesAction:)];
-    [self addGestureRecognizer:panGes];
 }
 
 -(void)typeButtonAction:(id)sender
@@ -84,10 +93,11 @@
     NSInteger n = (NSInteger)transformX / (_buttonWidth / 2);
 
     if (ges.state == UIGestureRecognizerStateEnded){
+        NSInteger titleCount = [self titleCount];
         if (n >= 0) {
                _backgroundView.transform = CGAffineTransformTranslate(_backgroundView.transform, -transformX, 0);
-          }else if (n < -(_modelCount - 1)){
-                _backgroundView.transform = CGAffineTransformTranslate(_backgroundView.transform, -transformX - _buttonWidth * (_modelCount - 1), 0);
+          }else if (n < -(titleCount - 1)){
+                _backgroundView.transform = CGAffineTransformTranslate(_backgroundView.transform, -transformX - _buttonWidth * (titleCount - 1), 0);
           }else{
               //判断滑动方向，进行处理
               NSInteger direction = touchPoint.x - _startPoint.x;
@@ -95,6 +105,11 @@
               _backgroundView.transform = CGAffineTransformTranslate(_backgroundView.transform, -transformX + _buttonWidth * m , 0);
           }
     }
+}
+
+-(NSInteger)titleCount
+{
+    return _titles.count;
 }
 
 @end
